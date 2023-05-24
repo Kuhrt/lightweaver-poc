253ce34549018979856fd1d57604739f9bcf3eef
Kuhrt Cowan <kuhrt.cowan@gmail.com>
2023-05-24 09:51:01 -0500

    KDC - Add actual images from mockup

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const user = await userApi.get();
 
   return (
-    <main className="relative flex-1 bg-dark min-h-full flex flex-col items-center justify-between overflow-hidden">
+    <main className="relative flex-1 bg-dark bg-dimensions-background bg-cover bg-no-repeat bg-center min-h-full flex flex-col items-center justify-between overflow-hidden">
       <Dimensions user={user} />
     </main>
   );
